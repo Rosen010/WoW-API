04024f9b933b65059b2b86729180f12a61f836c7
Rosen <Rosen010@mail.bg>
2022-01-29 21:37:31 +0200

    Add parameters constants and add method to interface

---
+++ WoW-API/BNetAPI.Core/Utilities/Constants/Parameters.cs
@@ -0,0 +1,27 @@
+﻿namespace BNetAPI.Core.Utilities.Constants
+{
+    public class Parameters
+    {
+        public static class Namespace
+        {
+            public const string ProfileEU = "profile-eu";
+
+            public const string ProfileUS = "profile-us";
+
+            public const string ProfileKR = "profile-kr";
+
+            public const string ProfileTW = "profile-tw";
+        }
+
+        public static class Locale
+        {
+            public const string GB = "en_GB";
+
+            public const string ES = "es_ES";
+
+            public const string RU = "ru_RU";
+
+            public const string DE = "de_DE";
+        }
+    }
+}
--- WoW-API/BNetAPI.Guilds/Interfaces/IBNetGuildClient.cs
+++ WoW-API/BNetAPI.Guilds/Interfaces/IBNetGuildClient.cs
@@ -1,6 +1,12 @@
-﻿namespace BNetAPI.Guilds.Interfaces
+﻿using BNetAPI.Core.Models;
+using BNetAPI.Guilds.Models.RequestModels;
+using BNetAPI.Guilds.Models.ResponseModels;
+using System.Threading.Tasks;
+
+namespace BNetAPI.Guilds.Interfaces
 {
     public interface IBNetGuildClient
     {
+        Task<GuildResponse> RetrieveGuild(AuthorizationData authData, GuildRequestModel request);
     }
 }
