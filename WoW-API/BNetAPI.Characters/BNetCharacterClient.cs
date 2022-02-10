0ff1d44bedc03e32a0ef13e85ff234038eea36f8
Rosen <Rosen010@mail.bg>
2022-02-10 23:15:16 +0200

    Implement character client.

--- WoW-API/BNetAPI.Characters/BNetCharacterClient.cs
+++ WoW-API/BNetAPI.Characters/BNetCharacterClient.cs
@@ -1,8 +1,47 @@
 ﻿using BNetAPI.Characters.Models.Interfaces;
+using BNetAPI.Characters.Models.RequestModels;
+using BNetAPI.Characters.Models.ResponseModels;
+using BNetAPI.Core.Enums;
+using BNetAPI.Core.Interfaces;
+using BNetAPI.Core.Utilities.Constants;
+using System.Net.Http.Headers;
+using System.Web;
 
 namespace BNetAPI.Characters
 {
     public class BNetCharacterClient : IBNetCharacterClient
     {
+        private readonly IBNetApiClient _apiClient;
+
+        private readonly IBNetRestClient _restClient;
+
+        public BNetCharacterClient(IBNetApiClient apiClient, IBNetRestClient restClient)
+        {
+            _apiClient = apiClient;
+            _restClient = restClient;
+        }
+
+        public async Task<CharacterResponse> RequestCharacter(CharacterRequestModel request)
+        {
+            var token = await _apiClient.FetchTokenAsync();
+            var endpoint = string.Format(Endpoints.Character, request.Realm, request.CharacterName);
+
+            var builder = new UriBuilder(endpoint);
+            var query = HttpUtility.ParseQueryString(builder.Query);
+
+            query[BNetRequestHeaders.Namespace.ToString().ToLower()] = request.NameSpace;
+            query[BNetRequestHeaders.Locale.ToString().ToLower()] = request.Locale;
+
+            builder.Query = query.ToString();
+            var url = builder.ToString();
+
+            using (var httpRequest = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                httpRequest.Headers.Authorization = new AuthenticationHeaderValue(ApiRequestConstants.AuthenticationType.Bearer, token);
+
+                var response = await _restClient.SendRequestAsync<CharacterResponse>(httpRequest);
+                return response;
+            }
+        }
     }
 }
--- WoW-API/BNetAPI.Core/Utilities/Constants/Endpoints.cs
+++ WoW-API/BNetAPI.Core/Utilities/Constants/Endpoints.cs
@@ -7,5 +7,7 @@
         public const string Guild = "https://eu.api.blizzard.com/data/wow/guild/{0}/{1}";
 
         public const string Roster = "https://eu.api.blizzard.com/data/wow/guild/{0}/{1}/roster";
+
+        public const string Character = "https://eu.api.blizzard.com/profile/wow/character/{0}/{1}";
     }
 }
