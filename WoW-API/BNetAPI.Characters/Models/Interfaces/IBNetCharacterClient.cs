76ac14be2cf356a9ca961a1f03f606ccc26b5a6b
Rosen <Rosen010@mail.bg>
2022-02-11 22:28:14 +0200

    Add RequestCharacter method to interface.

--- WoW-API/BNetAPI.Characters/Models/Interfaces/IBNetCharacterClient.cs
+++ WoW-API/BNetAPI.Characters/Models/Interfaces/IBNetCharacterClient.cs
@@ -1,6 +1,10 @@
-﻿namespace BNetAPI.Characters.Models.Interfaces
+﻿using BNetAPI.Characters.Models.RequestModels;
+using BNetAPI.Characters.Models.ResponseModels;
+
+namespace BNetAPI.Characters.Models.Interfaces
 {
     public interface IBNetCharacterClient
     {
+        Task<CharacterResponse> RequestCharacter(CharacterRequestModel request);
     }
 }
